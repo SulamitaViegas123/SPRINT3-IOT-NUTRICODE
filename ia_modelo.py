35342dd258845e9251605e7fdcb645391cce026e
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-30 12:05:23 -0300

    Primeiro commit NutriCode

---
+++ ia_modelo.py
@@ -0,0 +1,41 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import LabelEncoder
+
+# Carregar dados
+df = pd.read_csv("alimentos.csv")
+
+# 🔥 NORMALIZA COLUNAS
+df.columns = df.columns.str.upper().str.strip()
+
+print("Colunas:", df.columns)
+
+# Features
+X = df[["CALORIAS", "PROTEINA", "ACUCAR", "GORDURA", "CARBOIDRATO"]]
+
+# Target
+y = df["CLASSIFICACAO"]
+
+# Encoder
+le = LabelEncoder()
+y_encoded = le.fit_transform(y)
+
+# Split
+X_train, X_test, y_train, y_test = train_test_split(X, y_encoded, test_size=0.3, random_state=42)
+
+# Modelo
+modelo = DecisionTreeClassifier(max_depth=5)
+modelo.fit(X_train, y_train)
+
+# Avaliação
+accuracy = modelo.score(X_test, y_test)
+print(f"Acurácia: {accuracy:.2f}")
+
+# Teste
+import pandas as pd
+novo = pd.DataFrame([[200, 5, 20, 10, 30]],
+columns=["CALORIAS", "PROTEINA", "ACUCAR", "GORDURA", "CARBOIDRATO"])
+prev = modelo.predict(novo)
+
+print("Resultado:", le.inverse_transform(prev))
